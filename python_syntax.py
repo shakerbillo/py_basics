c585a3e3fd9d52f185e6e38d999e89c0ee487f08
Emmanuel Gyan-Amoah <emmanuelgyan-amoah@Emmanuels-MacBook-Pro.local>
2024-02-20 05:35:08 +0100

    initial commit

---
+++ python_syntax.py
@@ -0,0 +1,23 @@
+print('Hello'); 
+print('World')
+
+x = 1+    2 \
++ 3
+print(x)
+
+name = 'Emmanuel'
+if name == 'Emmanuel':
+    print(name);
+    
+    #any amount of whitespace on a single line is ok
+y     =        1        +        2
+print(y)
+
+# Commenting code
+
+# The function below accepts two integers and
+# adds both numbers to return the value.
+def add_two_numbers(a, b):
+    return a + b
+
+jazz = 1  # Resetting buffer size

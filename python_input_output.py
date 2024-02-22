513a34912c6963ddb5c85e8e036ada9a710596a1
Emmanuel Gyan-Amoah <emmanuelgyan-amoah@Emmanuels-MacBook-Pro.local>
2024-02-22 07:38:00 +0100

    input_output

---
+++ python_input_output.py
@@ -0,0 +1,8 @@
+num1 = input('Please enter the first number: ')
+num2 = input('Please enter a second number: ')
+print(type(num1), type(num2))
+print(int(num1) + int(num2))
+
+str1 = input('Please enter your first name: ')
+str2 = input('Please enter your last name: ')
+print('Hello {} {}'.format(str1, str2))
